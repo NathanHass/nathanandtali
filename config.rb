262d4c690bfbaa9a7eb9b2a3a92b1390e80f7672
Tito Bottitta <tito@upstatement.com>
2015-02-22 17:25:20 -0500

    First working prototype and commit of all files (fonts, images, style, scripts, etc.)

---
+++ config.rb
@@ -0,0 +1,29 @@
+# This is the config.rb file to support Upbase. Move this to your root folder after you bower install.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+
+# And this is where compass will watch and compile stuff when it runs:
+css_dir = "static/css/"
+sass_dir = "static/scss"
+images_dir = "static/img"
+javascripts_dir = "static/js"
+fonts_dir = "static/fonts"
+relative_assets = true
+
+# This line tells compass to look at the Upbase styles in your bower_components dir
+add_import_path "static/bower_components/Upbase/components"
+
+line_comments = false
+# output_style = :compact :compressed :nested :expanded
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+require 'autoprefixer-rails'
+
+on_stylesheet_saved do |file|
+  css = File.read(file)
+  File.open(file, 'w') do |io|
+    io << AutoprefixerRails.process(css)
+  end
+end
---
+++ static/js/base.js
@@ -0,0 +1,110 @@
+/*
+ * Do your jorbascrompts here!
+ */
+
+
+$(function() {
+
+  // --------------------------------------------
+  // Universal Variables
+  // --------------------------------------------
+
+  var $readNextNav = $('.readNextNav');
+  var $topicItem = $('.readNextNav-topicLink');
+
+
+  // --------------------------------------------
+  // Ellipsis
+  // --------------------------------------------
+
+  var forEach = Array.prototype.forEach;
+  var els = document.getElementsByClassName('readNextNav-storyTxt-bd');
+
+  forEach.call(els, function(el) {
+    var ellipsis = new Ellipsis(el);
+    ellipsis.calc();
+    ellipsis.set();
+  });
+
+
+  // --------------------------------------------
+  // Toggle Topic
+  // --------------------------------------------
+
+  $topicItem.on('click', function(){
+    event.preventDefault();
+
+    var $this = $(this);
+    var topicIsActive = $this.hasClass('is-active');
+    console.log(topicIsActive);
+
+    if (!topicIsActive) {
+      $topicItem.removeClass('is-active');
+      $this.addClass('is-active');
+      topicIsActive = false;
+    }
+  });
+
+
+  // --------------------------------------------
+  // Read Next Nav Dimmer
+  // --------------------------------------------
+
+  function dimNav(){
+      $readNextNav.addClass('is-dimmed');
+  };
+
+  function brightenNav(){
+      $readNextNav.removeClass('is-dimmed');
+  };
+
+
+  // Capture initial scroll position
+  var initialScroll = $(window).scrollTop();
+  var bottomThreshold = $(document).height() - $(window).height();
+
+  // Dim the nav only if initial scroll position changes
+  $(window).scroll(function(){
+    var scrollTop = $(window).scrollTop();
+
+    if (scrollTop > initialScroll && scrollTop != 0 && scrollTop < bottomThreshold ) {
+      dimNav();
+    } else {
+      brightenNav();
+    }
+  });
+
+  // Remove dimmer if user interacts with nav
+  $readNextNav.hover(brightenNav, dimNav);
+
+
+  // $(document).on( 'scroll', 'window', function(){
+  //     console.log('Event Fired');
+  // });
+
+  // function openTopics(event) {
+  //   event.preventDefault();
+  //   if (!topicsIsOpen) {
+  //     $body.addClass('topics-is-open');
+  //     topicsIsOpen = true;
+  //   }
+  // }
+
+  // function closeTopics(event) {
+  //   event.preventDefault();
+  //   if (topicsIsOpen)  {
+  //     $body.removeClass('topics-is-open');
+  //     topicsIsOpen = false;
+  //   }
+  // }
+
+  // function init() {
+
+  //   //$topicsButton.hover(openTopics, closeTopics);
+  // }
+
+  // return {
+  //   init: init
+  // };
+
+});
